298534e1bd7a49d0d0b54a03d19e9aea93ca318a
tatya <tatyana.k@gmx.net>
2025-03-10 16:37:58 +0100

    added types for  ProductFile and ProductCategory

--- src/features/products/types/index.ts
+++ src/features/products/types/index.ts
@@ -29,11 +29,57 @@ export interface Dimensions {
   width: number;
 }
 
+export interface ProductFile {
+  id: number;
+  productId: number;
+  originFileName: string;
+  fileUrl: string;
+}
+
+export interface NewProductCategoryDto{
+  name: string;
+  artName: string;
+}
+
+export interface NewProductDto{
+  name: string;
+  vendorArticle: string
+  purchasingPrice: number
+  productCategory: ProductCategory
+}
+
+export interface UpdateProductDto {
+  name?: string;
+  article?: string;
+  vendorArticle?: string;
+  purchasingPrice?: number;
+  sellingPrice?: number;
+  unitOfMeasurement?: string;
+  weight?: number;
+  newDimensions?: Dimensions;
+  productCategory?: ProductCategory;
+  description?: string;
+  customsNumber?: string;
+  // Добавляем индексную сигнатуру
+  [key: string]: string | number | Dimensions | ProductCategory | undefined;
+}
+
 export interface ProductsState {
   productsList: Product[];
   selectedProduct: Product | undefined;
   totalPages: number;
   currentPage: number;
+  loading: boolean;          
+  error: string | null;
+}
+
+export interface ProductCategoriesState {
+  productCategoriesList: ProductCategory[];
+  selectedProductCategory: ProductCategory | undefined;
+}
+
+export interface ProductFilesState {
+  files: ProductFile[];
 }
 
 // Интерфейс для ответа с пагинацией
