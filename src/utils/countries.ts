1b929ebc3c96b103f8594aab3dff4bf540895c72
tatya <tatyana.k@gmx.net>
2025-04-23 10:35:14 +0200

    created countries.ts

---
+++ src/utils/countries.ts
@@ -0,0 +1,15 @@
+
+export const countries = [
+    { code: "DE", name: "Germany" },
+    { code: "AT", name: "Austria" },
+    { code: "CH", name: "Switzerland" },
+    { code: "PL", name: "Poland" },
+    { code: "FR", name: "France" },
+    { code: "RU", name: "Russia" },
+    { code: "KZ", name: "Kazakhstan" },
+    { code: "CN", name: "Great Britain" },
+    { code: "IT", name: "Italy" },
+    { code: "ES", name: "Spain" },
+    // Можно добавить все страны по ISO 3166-1 или использовать библиотеку, как `country-list`
+  ];
+  
