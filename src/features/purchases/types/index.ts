217680e1d0daa19dc12a86f6edf7c712bfbdc41f
Tatyana-Kondr <tatyana.k@gmx.net>
2025-04-28 09:08:03 +0200

    added interface NewPurchaseDto and interface NewPurchaseItemDto

--- src/features/purchases/types/index.ts
+++ src/features/purchases/types/index.ts
@@ -15,11 +15,37 @@ export interface Purchase {
     purchaseItems: PurchaseItem[]
 }
 
+export interface NewPurchaseDto {
+    vendorId: number
+    purchasingDate: string
+    type: string
+    document: string
+    documentNumber: string
+    subtotal: number
+    taxSum: number
+    total: number
+    paymentStatus: string
+    purchaseItems: NewPurchaseItemDto[]
+}
+
 export interface PurchaseItem {
     id: number
     productId: Product
     productName: string
     productArticle: string
+    purchaseId: number
+    quantity: number
+    unitPrice: number
+    totalPrice: number
+    taxPercentage: number
+    taxAmount: number
+    totalAmount: number
+    position: number
+}
+
+export interface NewPurchaseItemDto {
+    productId: number
+    productName: string
     purchaseId: number
     quantity: number
     unitPrice: number
