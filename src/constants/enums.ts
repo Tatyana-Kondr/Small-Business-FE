ccfd2e42498a82de07096ab6087fdc9fe9fed512
tatya <tatyana.k@gmx.net>
2025-06-17 14:14:25 +0200

    added enums TypeOfDocument, Shipping, TermsOfPayment

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -31,3 +31,47 @@ export const PaymentStatuses: PaymentStatus[] = [
   "PENDING",
   "CANCELLED",
 ];
+
+// Тип документа
+export type TypeOfDocument =
+  | "RECHNUNG"
+  | "BON"
+  | "BESTELLUNG";
+
+export const TypesOfDocument: TypeOfDocument[] = [
+  "RECHNUNG",
+  "BON",
+  "BESTELLUNG",
+];
+
+// Доставка
+export type Shipping  =
+  | "DHL_PAKET"
+  | "POST_MAXI_BRIEF"
+  | "HERMES"
+  | "ASH_LOGISTIK_LUFTFRACHT_TRANSPORTE_ZOLLSERVICE"
+  | "ABHOLUNG";
+
+export const Shipping: Shipping[] = [
+ "DHL_PAKET",
+  "POST_MAXI_BRIEF",
+  "HERMES",
+  "ASH_LOGISTIK_LUFTFRACHT_TRANSPORTE_ZOLLSERVICE",
+  "ABHOLUNG"
+];
+
+//Вид оплаты счета
+export type TermsOfPayment =
+| "BETRAG_IM_BAR"
+| "ÜBERWEISUNG_7_TAGE_2_PROZENT_14_TAGE_NETTO"
+| "ÜBERWEISUNG_7_TAGE"
+| "ÜBERWEISUNG_14_TAGE"
+| "BETRAG_ERHALTEN_AM";
+
+export const TermsOfPayment: TermsOfPayment[] = [
+"BETRAG_IM_BAR",
+"ÜBERWEISUNG_7_TAGE_2_PROZENT_14_TAGE_NETTO",
+"ÜBERWEISUNG_7_TAGE",
+"ÜBERWEISUNG_14_TAGE",
+"BETRAG_ERHALTEN_AM"
+];
