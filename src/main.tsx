d47a2091c414164a3352cc07b2c908662e81dcc4
tatya <tatyana.k@gmx.net>
2025-08-02 10:58:40 +0200

    added theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,26 @@ import { store } from "./redux/store";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme.ts";
 
 const container = document.getElementById("root")
 
 if (container) {
   const root = createRoot(container)
 
-  root.render(
+ root.render(
     <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-)
+      <Provider store={store}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
 } else {
 throw new Error(
   "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
