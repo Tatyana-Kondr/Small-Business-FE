52e0664ceb28b1f75805a3362bc46404e0bc7a5a
tatya <tatyana.k@gmx.net>
2025-10-16 13:56:15 +0200

    deleted button Zurück

--- src/features/payments/components/paymentMetods/PaymentMethodsList.tsx
+++ src/features/payments/components/paymentMetods/PaymentMethodsList.tsx
@@ -1,8 +1,6 @@
 import { Box, Container, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Button, Checkbox } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../../../redux/hooks";
 import { useEffect, useState } from "react";
-import KeyboardDoubleArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowLeftOutlined';
-import { useNavigate } from "react-router-dom";
 import { getPaymentMethods, selectPaymentMethods, updatePaymentMethod } from "../../paymentMethodsSlice";
 import { NewPaymentMethodDto, PaymentMethod } from "../../types";
 import DeletePaymentMethod from "./DeletePaymentMethod";
@@ -30,7 +28,6 @@ export default function PaymentMethodsList() {
     const dispatch = useAppDispatch();
     const paymentMethods = useAppSelector(selectPaymentMethods);
     const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<PaymentMethod | null>(null);
-    const navigate = useNavigate();
     const [openModal, setOpenModal] = useState(false);
     const [editOpen, setEditOpen] = useState(false);
 
@@ -64,38 +61,9 @@ const handleToggleActive = async (method: PaymentMethod) => {
   }
 };
 
-    const handleGoBack = () => {
-        navigate(-1)
-    }
-
     return (
         <Container>
-         <Button
-                    onClick={handleGoBack}
-                    sx={{
-                        fontSize: 12,
-                        minWidth: 40,
-                        minHeight: 40,
-                        padding: 0,
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        borderRadius: 1,
-                        backgroundColor: "transparent",
-                        "&:hover": {
-                            backgroundColor: "transparent", // фон не меняется при ховере
-                            "& .MuiSvgIcon-root": {
-                                color: "#00838f", // цвет иконки при наведении
-                            },
-                        },
-                        "& .MuiSvgIcon-root": {
-                            transition: "color 0.3s ease", // плавный переход цвета
-                        },
-                    }}
-                >
-                    <KeyboardDoubleArrowLeftOutlinedIcon fontSize="large" /> 
-                    ZURÜCK
-                </Button>
+         
             {/* Верхняя панель */}
             <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
                 <Typography sx={{ textAlign: "left", fontWeight: "bold", textDecoration: 'underline', color: "#0277bd" }}>ZAHLUNGSMETHODEN</Typography>
@@ -122,9 +90,9 @@ const handleToggleActive = async (method: PaymentMethod) => {
                             paymentMethods.map((method) => (
                                 <StyledTableRow key={method.id}>
                                     <TableCell style={{ padding: "6px 12px", borderRight: "1px solid #ddd", borderLeft: "1px solid #ddd" }}>{method.id}</TableCell>
-                                    <TableCell sx={{ width: "400px", padding: "6px 12px", borderRight: "1px solid #ddd" }}>{method.provider}</TableCell>
-                                    <TableCell sx={{ width: "100px", padding: "6px 12px", borderRight: "1px solid #ddd" }}>{method.maskedNumber}</TableCell>
-                                    <TableCell sx={{ width: "100px", padding: "6px 12px", borderRight: "1px solid #ddd" }}>{method.details}</TableCell>
+                                    <TableCell sx={{ minWidth: "200px", padding: "6px 12px", borderRight: "1px solid #ddd" }}>{method.provider}</TableCell>
+                                    <TableCell sx={{ padding: "6px 12px", borderRight: "1px solid #ddd" }}>{method.maskedNumber}</TableCell>
+                                    <TableCell sx={{ padding: "6px 12px", borderRight: "1px solid #ddd" }}>{method.details}</TableCell>
                                     <TableCell sx={{ textAlign: "center", borderRight: "1px solid #ddd" }}>
                                         <Checkbox
                                             checked={method.active}
