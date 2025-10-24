339a657c1078a473ed241c0e85c1aba3bdd24445
tatya <tatyana.k@gmx.net>
2025-10-24 09:54:31 +0200

    deleted type UnitOfMeasurement

--- src/constants/unitOfMeasurements.ts
+++
@@ -1,3 +0,0 @@
-export const unitOfMeasurements = ["KG", "GR", "LITER", "METER", "ST", "STUNDE", "SET", "MM", "CM", "KM"] as const;
-
-export type UnitOfMeasurement = (typeof unitOfMeasurements)[number]; 
