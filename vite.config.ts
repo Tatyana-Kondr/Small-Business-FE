09d99b51755495920795b6f7b04e57c520210f7e
tatya <tatyana.k@gmx.net>
2025-11-05 18:03:11 +0100

    fixed .env and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080", // Адрес вашего бэкенда
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // автоматически берёт из .env.development
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  };
+});
 
