665d565cd7b489338ad32aefca98216419bf4295
tatya <tatyana.k@gmx.net>
2025-11-26 18:50:25 +0100

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "./",   // <<< ВАЖНО: делает пути относительными!
-    
+    base: "./", // делает пути относительными
+
     plugins: [react()],
-    
+
+    build: {
+      sourcemap: false,                  // отключаем source maps для продакшена
+      chunkSizeWarningLimit: 2000,       // увеличиваем лимит, чтобы не ругался
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom"],
+            mui: ["@mui/material", "@mui/icons-material"],
+            dayjs: ["dayjs"],
+            vendor: [
+              "react-router-dom",
+              "axios",
+              "@reduxjs/toolkit",
+              "react-redux"
+            ]
+          }
+        }
+      }
+    },
+
     server: {
       proxy: {
         "/api": {
@@ -20,4 +40,3 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-
