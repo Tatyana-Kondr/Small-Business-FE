c567eadc916b1eabae6c66f7d23e57eebb178165
tatya <tatyana.k@gmx.net>
2026-08-24 20:12:46 +0200

    fixed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme.ts";
+import { theme } from "./theme/theme.ts";
+
 
 const container = document.getElementById("root")
 
@@ -14,17 +15,19 @@ if (container) {
   const root = createRoot(container)
 
  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
         <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-          </ThemeProvider>
+          <App />
         </BrowserRouter>
-      </Provider>
-    </React.StrictMode>
-  );
+
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
 } else {
 throw new Error(
   "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
